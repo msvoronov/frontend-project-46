300311748f02caeffd7921bac78d1176a0a17b14
Mikhail Voronov <msvoronov@inbox.ru>
2024-09-23 20:38:15 +0300

    update gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
-import { program } from "commander";
+import { program } from 'commander';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')  
+  .version('1.0.0')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file');
 
-program.parse(process.argv)
+program.parse(process.argv);
