4fff25700b9c26e507bc50750bee611b10f15b5e
Mikhail Voronov <msvoronov@inbox.ru>
2024-09-26 12:16:21 +0300

    create getDifference.js

---
+++ src/getDifference.js
@@ -0,0 +1,47 @@
+const toSring = (object) => {
+  const entries = Object.entries(object);
+  const string = entries.reduce((acc, entrie) => {
+    const [key, value] = entrie;
+    return `${acc}\n  ${key}: ${value}`;
+  }, '');
+  return `\n{${string}\n}`;
+};
+/*
+const getDifference = (obj1, obj2) => {
+  const result = {};
+  const allKeys = Object.keys({ ...obj1, ...obj2 });
+  for (const key of allKeys) {
+    if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
+      result[`- ${key}`] = obj1[key];
+    } else if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
+      result[`+ ${key}`] = obj2[key];
+    } else if (obj1[key] === obj2[key]) {
+      result[key] = obj1[key];
+    } else {
+      result[`- ${key}`] = obj1[key];
+      result[`+ ${key}`] = obj2[key];
+    }
+  }
+  return toSring(result);
+};
+*/
+const getDifference = (obj1, obj2) => {
+  const allKeys = Object.keys({ ...obj1, ...obj2 });
+  const difference = allKeys.reduce((acc, key) => {
+    const result = {};
+    if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
+      result[`- ${key}`] = obj1[key];
+    } else if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
+      result[`+ ${key}`] = obj2[key];
+    } else if (obj1[key] === obj2[key]) {
+      result[`  ${key}`] = obj1[key];
+    } else {
+      result[`- ${key}`] = obj1[key];
+      result[`+ ${key}`] = obj2[key];
+    }
+    return { ...acc, ...result };
+  }, {});
+  return toSring(difference);
+};
+
+export default getDifference;
