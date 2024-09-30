26e1d37d0e52e79b52fba1117fc335dde203136b
Mikhail Voronov <msvoronov@inbox.ru>
2024-09-30 10:10:03 +0300

    added parse .yaml-files

--- src/parseFile.js
+++ src/parseFile.js
@@ -1,14 +1,17 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 import { cwd } from 'process';
+import jsYaml from 'js-yaml';
 
-const getPath = (fileName) => path.resolve(cwd(), 'src/', `./${fileName}`);
+const getPath = (fileName) => path.resolve(cwd(), 'src/files/', `./${fileName}`);
 
 const parseFile = (fileName) => {
+  const extname = path.extname(fileName);
   const filePath = getPath(fileName);
-  const file = readFileSync(filePath);
-  const fileContents = JSON.parse(file); // Формат данных определяйте на основе расширения файла !!!
-  return fileContents;
+  const file = extname === '.json'
+    ? JSON.parse(readFileSync(filePath))
+    : jsYaml.load(readFileSync(filePath));
+  return file;
 };
 
 export default parseFile;
