a6ed72f3bba27fb5b4562469aab6a6bc4bd25b63
Mikhail Voronov <msvoronov@inbox.ru>
2024-10-03 17:07:41 +0300

    update src/ files

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,29 +1,26 @@
 import { program } from 'commander';
-import parseFile from './parseFile.js';
-import convertObjToStr from './convertObjToStr.js';
-import getDifference from './getDifference.js';
+import { getObject, getDifference, getAnswerStylish } from './index.js';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .argument('<filepath1>', 'path to first file')
   .argument('<filepath2>', 'path to second file')
   .action(() => {
-    const { args } = program; // получаем массив строк - переданных аргументов (имен файлов)
-    const objects = args.map(parseFile); // преобразуем в массив объектов
-    const sortedObjects = objects
-      .map((obj) => Object.entries(obj).sort()) // разибраем на подмассивы и сортируем
-      .map((arr) => Object.fromEntries(arr)); // собираем обратно в объекты
-    const [obj1, obj2] = sortedObjects;
+    const { format } = program.opts(); // Переданная опция
+    const { args } = program; // Массив имен файлов (переданных в аргументы утилиты)
+    const objects = args.map(getObject); // получаем из файлов объекты
+    const [obj1, obj2] = objects;
 
     const difference = getDifference(obj1, obj2);
-    console.log(convertObjToStr(difference));
-
-    // Объект { название: 'значение' } с переданными опциями
-    // const options = program.opts();
+    if (format === 'stylish') {
+      console.log(getAnswerStylish(difference));
+    } else {
+      console.log('other format');
+    }
   });
 
 const runGenDiff = () => {
--- src/parseFile.js
+++ src/parseFile.js
@@ -1,13 +1,9 @@
 import { readFileSync } from 'fs';
 import path from 'path';
-import { cwd } from 'process';
 import jsYaml from 'js-yaml';
 
-const getPath = (fileName) => path.resolve(cwd(), 'src/files/', `./${fileName}`);
-
-const parseFile = (fileName) => {
-  const extname = path.extname(fileName);
-  const filePath = getPath(fileName);
+const parseFile = (filePath) => {
+  const extname = path.extname(filePath);
   const file = extname === '.json'
     ? JSON.parse(readFileSync(filePath))
     : jsYaml.load(readFileSync(filePath));
