ff783f8420b7a60af6ebae3024707acede3e05d2
Mikhail Voronov <msvoronov@inbox.ru>
2024-10-06 20:58:29 +0300

    fix lint issues

--- src/formetters/json.js
+++ src/formetters/json.js
@@ -1,10 +1,3 @@
 const getJSON = (object) => JSON.stringify(object, null, 2);
 
 export default getJSON;
-
-/*
-gendiff -h
-gendiff file1.json file2.yml
-gendiff -f plain file1.json file2.yml
-gendiff --format json file1.json file2.yml
-*/
